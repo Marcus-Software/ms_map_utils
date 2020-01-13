af9b925751636404740286a41527c9ecebddd65a
Marcus Duarte <marcusedu7@gmail.com>
2020-01-13 10:28:25 -0200

    compact and trim functions implemented

--- lib/ms_map_utils.dart
+++ lib/ms_map_utils.dart
@@ -1,7 +1,37 @@
-library ms_map_utils;
+/// Return a new map without null values
+Map<K, V> compact<K, V>(Map map, [bool newMap = false]) =>
+    (newMap ? Map<K, V>.from(map) : map)
+      ..removeWhere((_, value) {
+        if (value is Map && value != null) {
+          compact(value, newMap);
+        } else if (value is List<Map> && value != null) {
+          value.forEach((mapInList) {
+            compact(mapInList, newMap);
+          });
+        }
+        return value == null;
+      });
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+/// Trim every value is a String if recursive with map and lists with map
+Map<K, V> trim<K, V>(Map map, [bool newMap = false]) {
+  return (newMap ? Map<K, V>.from(map) : map).map((key, value) {
+    var tmpValue = value;
+    if (value is String) {
+      tmpValue = value.trim();
+    } else if (value is Map) {
+      tmpValue = trim(value);
+    } else if (value is List<Map>) {
+      tmpValue = value.cast<Map>().map((vMap) => trim(vMap, newMap)).toList();
+    }
+    return MapEntry(key, tmpValue);
+  });
+}
+
+Function _compact = compact;
+Function _trim = trim;
+
+extension of on Map {
+  Map compact([bool newMap = false]) => _compact(this, newMap);
+
+  Map trim([bool newMap = false]) => _trim(this, newMap);
 }
--- test/ms_map_utils_test.dart
+++ test/ms_map_utils_test.dart
@@ -1,13 +1,116 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:ms_map_utils/ms_map_utils.dart';
+import 'package:test/test.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
-    expect(() => calculator.addOne(null), throwsNoSuchMethodError);
+  group("Compact Map", () {
+    test('Must return a new empty HashMap without null values', () {
+      var mapWithNullValues = {
+        "k1": null,
+        "k2": null,
+        "k3": null,
+        'map': {
+          "k1": null,
+          "k2": null,
+          "k3": null,
+        },
+        'list': [
+          {
+            "k1": null,
+            "k2": null,
+            "k3": null,
+          },
+          {
+            "k1": null,
+            "k2": null,
+            "k3": null,
+          },
+        ]
+      };
+      var mapWithoutNull = compact(mapWithNullValues);
+      expect(mapWithoutNull, {
+        'map': {},
+        'list': [{}, {}]
+      });
+    });
+    test('Must return a new HashMap without null values', () {
+      var mapWithNullValues = {
+        "k1": null,
+        "k2": null,
+        "k3": null,
+        "k4": "any value"
+      };
+      var mapWithoutNull = compact(mapWithNullValues);
+      expect(mapWithoutNull.length, 1);
+      expect(mapWithoutNull, {"k4": "any value"});
+    });
+    test('Must return a new HashMap without null values', () {
+      var mapWithNullValues = {
+        "k1": null,
+        "k2": null,
+        "k3": null,
+        "k4": "any value"
+      };
+      var mapWithoutNull = mapWithNullValues.compact(true);
+      expect(mapWithoutNull.length, 1);
+      expect(mapWithoutNull, {"k4": "any value"});
+    });
+  });
+  group("Trim Strings on map", () {
+    test("Most trim any Strings values", () {
+      const mapToTrim = {
+        "key1": "  random string    ",
+        "key2": "       another random string       ",
+        "key3": 321
+      };
+      expect(mapToTrim.trim(true), {
+        "key1": "random string",
+        "key2": "another random string",
+        "key3": 321
+      });
+    });
+    test("Most trim any Strings values and map enhered", () {
+      const mapToTrim = {
+        "key1": "  random string    ",
+        "key2": "       another random string       ",
+        "map": {
+          "key1": "  random string    ",
+          "key2": "       another random string       ",
+          "map": {
+            "key1": "  random string    ",
+            "key2": "       another random string       ",
+          }
+        }
+      };
+      expect(mapToTrim.trim(true), {
+        "key1": "random string",
+        "key2": "another random string",
+        "map": {
+          "key1": "random string",
+          "key2": "another random string",
+          "map": {
+            "key1": "random string",
+            "key2": "another random string",
+          }
+        }
+      });
+    });
+  });
+  group("Must trim list with maps", () {
+    test("Trim a map with a list of maps", () {
+      var mapToTrim = {
+        "key": [
+          {"key": " random string  "},
+          {"key": "   another random string      "},
+          {"key": "  last random string    "},
+        ]
+      };
+      expect(mapToTrim.trim(true), {
+        "key": [
+          {"key": "random string"},
+          {"key": "another random string"},
+          {"key": "last random string"},
+        ]
+      });
+    });
   });
 }
